a6ae5155b1d824af07bda16dce7da6cb5c0f7cf0
Artem Tseranu <artemtseranu@fastmail.com>
2018-08-13 11:53:53 +0300

    Set initBlockNumber on Ethereum state init

--- client/src/lib/ethereum_utils.js
+++ client/src/lib/ethereum_utils.js
@@ -2,11 +2,28 @@ export function checkWeb3() {
   return window.web3 !== undefined;
 }
 
-export function currentAccount() {
+function requireWeb3() {
   if (!checkWeb3()) {
     const error = { message: 'web3 is required' };
     throw error;
   }
+}
+
+export function currentAccount() {
+  requireWeb3();
 
   return window.web3.eth.accounts[0];
+}
+
+export function getBlockNumber() {
+  return new Promise((resolve, reject) => {
+    window.web3.eth.getBlockNumber((error, blockNumber) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+
+      resolve(blockNumber);
+    });
+  });
 }
--- client/src/sagas/ethereum.js
+++ client/src/sagas/ethereum.js
@@ -3,6 +3,7 @@ import {
 } from 'redux-saga/effects';
 import TruffleContract from 'truffle-contract';
 
+import { getBlockNumber } from 'Lib/ethereum_utils';
 import * as Events from 'Events/ethereum';
 
 import offersContractArtifact from 'ContractArtifacts/Offers.json';
@@ -27,11 +28,22 @@ function* init() {
     return;
   }
 
+  let initBlockNumber;
+
+  try {
+    initBlockNumber = yield call(getBlockNumber);
+  } catch (error) {
+    const errorMessage = `Failed to get current block number. ${error.message}`;
+    yield put({ type: Events.Init.FAILED, errorMessage });
+    return;
+  }
+
   yield put({
     type: Events.Init.SUCCEEDED,
     contracts: {
       offers: offersContract,
     },
+    initBlockNumber,
   });
 }
 
