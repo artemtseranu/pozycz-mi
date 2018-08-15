49948b4e096cda96bab6c7c099ef29036fb773ae
Artem Tseranu <artemtseranu@fastmail.com>
2018-08-15 11:57:15 +0300

    Remove linked list logic from Offers contract

--- contracts/Offers.sol
+++ contracts/Offers.sol
@@ -3,17 +3,13 @@ pragma solidity ^0.4.23;
 contract Offers {
   struct Offer {
     address owner;
-    bool isCreated;
-    bool isOpen;
     string description;
     bytes32 details;
-    uint prev;
-    uint next;
+    bool isOpen;
   }
 
-  mapping(uint => Offer) list;
-  uint public head;
-  uint public tail;
+  mapping(uint => Offer) public offers;
+  uint offersIdSeq = 0;
 
   event OfferCreated(address indexed owner, uint id, string description, bytes32 details);
 
@@ -22,44 +18,21 @@ contract Offers {
 
     Offer memory offer = Offer({
       owner: owner,
-      isOpen: false,
       description: description,
       details: details,
-      isCreated: true,
-      prev: tail,
-      next: 0
+      isOpen: false
     });
 
-    uint id = tail + 1;
-    list[id] = offer;
-
-    if (list[tail].isCreated) {
-      list[tail].next = id;
-    } else {
-      head = id;
-    }
-
-    tail = id;
-
+    uint id = offersIdSeq + 1;
+    offers[id] = offer;
     emit OfferCreated({owner: owner, id: id, description: description, details: details});
   }
 
-  function updateOfferDetails(uint id, bytes32 details) public {
-    Offer storage offer = list[id];
+//   function updateOfferDetails(uint id, bytes32 details) public {
+//     Offer storage offer = offers[id];
 
-    require(offer.owner == msg.sender, "An offer can be updated only by its owner");
+//     require(offer.owner == msg.sender, "An offer can be updated only by its owner");
 
-    offer.details = details;
-  }
-
-  function getOffer(uint id) public view returns(address owner, string description, bytes32 details, bool isOpen, uint prev, uint next) {
-    return (
-      list[id].owner,
-      list[id].description,
-      list[id].details,
-      list[id].isOpen,
-      list[id].prev,
-      list[id].next
-    );
-  }
+//     offer.details = details;
+//   }
 }
